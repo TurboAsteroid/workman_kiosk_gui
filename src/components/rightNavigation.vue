5952144e4bef975153ba8ed4c9cf57158520d162
nightwelf <bpo@elem.ru>
2019-11-22 09:26:42 +0500

    init5

--- src/components/rightNavigation.vue
+++ src/components/rightNavigation.vue
@@ -1,7 +1,35 @@
 <template>
-  <v-container>
-    rightNavigation
-  </v-container>
+  <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
+      color="deep-orange darken-1"
+  >
+    <v-list-item @click="$emit('selected', 1)">
+      <v-list-item-content>
+        <v-list-item-title>Single-line item</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item two-line @click="$emit('selected', 2)">
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item three-line @click="$emit('selected', 3)">
+      <v-list-item-content>
+        <v-list-item-title>Three-line item</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
 </template>
 
 <script>
