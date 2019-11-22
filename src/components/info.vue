c263c29d1c81f226ab8bd3adb59cda83dfda9bae
lvo <vlobarev@gmail.com>
2019-11-22 15:39:05 +0500

    slider fix

--- src/components/info.vue
+++ src/components/info.vue
@@ -22,8 +22,9 @@
       <v-tab
         style="width: 94px;"
         v-for="(item, i) in images"
-        :key="'#tab-' + item.icon"
-        :href="'#tab-' + i">
+        :key="'#tab-' + i"
+        :href="'#tab-' + i"
+        @click=changePage(i)>
         <img :src="item.icon">
       </v-tab>
     </v-tabs>
@@ -34,57 +35,69 @@
 export default {
   name: 'info',
   data: () => ({
-    images: {
-      1: {
+    image: 0,
+    interval: null,
+    images: [
+      {
         img: require('@/assets/img/1.jpg'),
         icon: require('@/assets/img/1_small.jpg')
       },
-      2: {
+      {
         img: require('@/assets/img/2.jpg'),
         icon: require('@/assets/img/2_small.jpg')
       },
-      3: {
+      {
         img: require('@/assets/img/3.jpg'),
         icon: require('@/assets/img/3_small.jpg')
       },
-      4: {
+      {
         img: require('@/assets/img/4.jpg'),
         icon: require('@/assets/img/4_small.jpg')
       },
-      5: {
+      {
         img: require('@/assets/img/5.jpg'),
         icon: require('@/assets/img/5_small.jpg')
       },
-      6: {
+      {
         img: require('@/assets/img/6.jpg'),
         icon: require('@/assets/img/6_small.jpg')
       },
-      7: {
+      {
         img: require('@/assets/img/7.jpg'),
         icon: require('@/assets/img/7_small.jpg')
       },
-      8: {
+      {
         img: require('@/assets/img/8.jpg'),
         icon: require('@/assets/img/8_small.jpg')
       },
-      9: {
+      {
         img: require('@/assets/img/9.jpg'),
         icon: require('@/assets/img/9_small.jpg')
       },
-      10: {
+      {
         img: require('@/assets/img/10.jpg'),
         icon: require('@/assets/img/10_small.jpg')
       }
-    },
-    tab: null,
+    ],
   }),
 	mounted() {
-      const self = this
-      setInterval(() => {
-          let currentDate = new Date()
-          self.tab = 'tab-' + Math.round(currentDate.getSeconds()/6)
-			}, 1000)
-	}
+    this.setInterval()
+	},
+  methods: {
+    changePage(index = this.image + 1) {
+      clearInterval(this.interval)
+      this.image = (index %this.images.length)
+      this.setInterval()
+    },
+    setInterval() {
+      this.interval = setInterval(() => this.changePage(), 6000)
+    }
+  },
+  computed: {
+    tab() {
+      return 'tab-' + this.image
+    }
+  }
 }
 </script>
 <style scoped>
