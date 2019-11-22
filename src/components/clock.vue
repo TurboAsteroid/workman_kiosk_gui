9b7ed90130cfbc98b336d14a2bf389fb6903987e
lvo <vlobarev@gmail.com>
2019-11-22 14:14:02 +0500

    clock

--- src/components/clock.vue
+++ src/components/clock.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
-    clock
-  </v-container>
+  <div id="time">
+    {{ time }}
+  </div>
 </template>
 
 <script>
@@ -9,8 +9,30 @@ export default {
   name: 'clock',
   props: {
     url: String
-  }
+  },
+  data: () => ({
+    time: '',
+  }),
+  mounted: function() {
+    setInterval(() => {
+      let currentDate = new Date()
+      let currentMinutes = currentDate.getMinutes()
+      let currentSeconds = currentDate.getSeconds()
+      this.time = `${currentDate.getHours()}${currentSeconds%2?' ':':'}${currentMinutes < 10 ? '0' : ''}${currentMinutes}`
+    }, 1000)
+  },
 }
 </script>
 <style scoped>
+  #time {
+    color: #f39322;
+    height: 70px;
+    text-align: center;
+    font-size: 3.5em;
+    font-weight: bold;
+    border-radius: 10px;
+    background-color: #802010;
+    line-height: 70px;
+    font-family: monospace;
+  }
 </style>
